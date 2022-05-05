d83dbc8138d47311193dc6cefeba5e7beda7e664
4537648 <49045557+4537648@users.noreply.github.com>
2022-05-05 22:26:22 +0300

    Courier covered by tests 4

--- src/main/java/ru/yandex/praktikum/courier/Courier.java
+++ src/main/java/ru/yandex/praktikum/courier/Courier.java
@@ -1,6 +1,5 @@
 package ru.yandex.praktikum.courier;
 
-import groovyjarjarantlr4.v4.codegen.model.SrcOp;
 import lombok.Data;
 import org.apache.commons.lang3.RandomStringUtils;
 
@@ -9,7 +8,6 @@ public class Courier {
   private String login;
   private String password;
   private String firstName;
-
 
   public Courier(String login, String password, String firstName) {
     this.login = login;
@@ -21,6 +19,27 @@ public class Courier {
     String login = RandomStringUtils.randomAlphanumeric(10);
     String password = RandomStringUtils.randomAlphanumeric(10);
     String firstName = RandomStringUtils.randomAlphanumeric(10);
+    return new Courier(login, password, firstName);
+  }
+
+  public static Courier getRandomWithoutLogin() {
+    String login = null;
+    String password = RandomStringUtils.randomAlphanumeric(10);
+    String firstName = RandomStringUtils.randomAlphanumeric(10);
+    return new Courier(login, password, firstName);
+  }
+
+  public static Courier getRandomWithoutPassword() {
+    String login = RandomStringUtils.randomAlphanumeric(10);
+    String password = null;
+    String firstName = RandomStringUtils.randomAlphanumeric(10);
+    return new Courier(login, password, firstName);
+  }
+
+  public static Courier getRandomWithoutFirstName() {
+    String login = RandomStringUtils.randomAlphanumeric(10);
+    String password = RandomStringUtils.randomAlphanumeric(10);
+    String firstName = null;
     return new Courier(login, password, firstName);
   }
 }
